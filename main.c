55b06024ae8356dc12b7a67ed472ed7b61d22f84
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-10-07 20:57:05 +0900

    04_P5

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  04_P4
+//  04_P5
 //
 //  Created by ok sojoung on 2023/10/05.
 //
@@ -9,15 +9,21 @@
 #include <stdlib.h>
 
 int main(void) {
-    int year;
+    unsigned int x;
+    int b;
     
-    printf("input a year:");
-    scanf("%d", &year);
+    printf("input a number :");
+    scanf("%ui", &x);
     
-    if ( (year%4==0 && year%100!=0) || year%400==0 )
-        printf("This is leap year\n");
-    else
-        printf("This is not leap year\n");
+    for (b=0; x!=0; x >>= 1)
+    {
+        if(x&1)
+        {
+            b++;
+        }
+    }
+    
+    printf("The result is %i\n",b);
     
     return 0;
 }
