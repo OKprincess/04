aaeeb71aafd1870303083fdd34154f452f4703a8
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-10-07 20:39:56 +0900

    04_P2

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  04_P1
+//  04_P2
 //
 //  Created by ok sojoung on 2023/10/05.
 //
@@ -8,19 +8,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    int x, y, z, m;
-    int a, b, c;
-    x = 2;
-    z = 1;
-    a = 3;
-    b = 4;
-    c = 5;
+int main(void) {
+    int a, b;
+    int sum, diff, mul, por, rest;
     
-    y = (a*x*x + b*x + c);
-    m = ((x + y + z)/3);
+    printf("input two integers :");
+    scanf("%i %i", &a, &b);
     
-    printf("y=%d, m=%d\n", y, m);
+    sum     = a+b;
+    diff    = a-b;
+    mul     = a*b;
+    por     = a/b;
+    rest    = a%b;
+    
+    printf("+ result is %d.\n", sum);
+    printf("- result is %d.\n", diff);
+    printf("* result is %d.\n", mul);
+    printf("/ result is %d.\n", por);
+    printf("%% result is %d.\n", rest);
     
     return 0;
 }
